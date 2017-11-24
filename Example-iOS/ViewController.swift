440d34d48a828c7f6895915c2ab855c61a1ebb9c
Dmitry Volosach <valueincorporation@gmail.com>
2017-11-24 11:53:20 +0300

    added Example-iOS target

---
+++ Example-iOS/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Example-iOS
+//
+//  Created by Dmitry on 11/24/17.
+//
+
+import UIKit
+import Pooling
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // TODO
+    }
+}
+
